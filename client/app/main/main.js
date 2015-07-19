a4b0c1939640ea2ffc2164040e27ef6d78b382c8
Jinyoung Kim <ligeek49@gmail.com>
2015-07-19 15:37:01 +0900

    add Sidenav demo code

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -6,6 +6,7 @@ angular.module('learntubeApp')
       .state('Home', {
         url: '/',
         templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
+        //controller: 'MainCtrl'
+        controller: 'AppCtrl'
       });
   });
