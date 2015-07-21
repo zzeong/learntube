ee69f9dcc82ad4955336f98033f1f3d724cf6dd5
Jinyoung Kim <ligeek49@gmail.com>
2015-07-21 14:40:58 +0900

    implement back navigation at Toolbar
    [Finishes #99200548]

--- client/components/toolbar/toolbar.controller.js
+++ client/components/toolbar/toolbar.controller.js
@@ -1,5 +1,16 @@
 'use strict';
 
 angular.module('learntubeApp')
-  .controller('ToolbarCtrl', function ($scope, $location, Auth) {
+  .controller('ToolbarCtrl', function ($scope, $location, $state, $window) {
+    var path = {
+      menu: '/assets/images/menu.svg',
+      back: '/assets/images/back.svg',
+    }
+
+    $scope.leftIconChanger = function() {
+      return $state.current.url === '/' ? path.menu : path.back;
+    };
+    $scope.back = function() {
+      $window.history.back(); 
+    };
   });
