1f45705412a9c48f06a2ee77e3950346eba16b9a
Jinyoung Kim <ligeek49@gmail.com>
2015-07-21 18:07:34 +0900

    hide search icon on Toolbar where main page

--- client/components/toolbar/toolbar.controller.js
+++ client/components/toolbar/toolbar.controller.js
@@ -1,18 +1,29 @@
 'use strict';
 
 angular.module('learntubeApp')
-  .controller('ToolbarCtrl', function ($scope, $location, $state, $window) {
-    var path = {
-      menu: '/assets/images/menu.svg',
-      back: '/assets/images/back.svg',
-    }
+.controller('ToolbarCtrl', function ($scope, $location, $state, $window) {
+  var curUrl = $state.current.url,
+    path = {
+    menu: '/assets/images/menu.svg',
+    back: '/assets/images/back.svg',
+  };
 
-    $scope.title = $state.current.name;
 
-    $scope.leftIconChanger = function() {
-      return $state.current.url === '/' ? path.menu : path.back;
-    };
-    $scope.back = function() {
-      $window.history.back(); 
-    };
-  });
+  $scope.title = $state.current.name;
+
+  $scope.isHome = function() {
+    return curUrl === '/' ? true : false;
+  };
+  $scope.leftIconChanger = function() {
+    return $scope.isHome() ? path.menu : path.back;
+  };
+  $scope.back = function() {
+    $window.history.back(); 
+  };
+})
+.config( function($mdThemingProvider){
+  // Configure a dark theme with primary foreground yellow
+  $mdThemingProvider.theme('docs-dark', 'default')
+  .primaryPalette('yellow')
+  .dark();
+});
