90938745d53014336a080aeef127f2760ad6a59c
Jinyoung Kim <ligeek49@gmail.com>
2015-07-23 16:59:13 +0900

    implement sidenav feature

--- client/components/sidenav/sidenav.controller.js
+++ client/components/sidenav/sidenav.controller.js
@@ -1,6 +1,31 @@
 'use strict';
 
 angular.module('learntubeApp')
+.controller('SidenavDrawerCtrl', function($scope, Auth, $location) {
+  $scope.isLoggedIn = Auth.isLoggedIn;
+  $scope.isAdmin = Auth.isAdmin;
+  $scope.getCurrentUser = Auth.getCurrentUser;
+  $scope.personalList = ['Watched Contents', 'Uploaded Contents', 'Settings', 'Log out'];
+
+  console.log($scope.getCurrentUser());
+  $scope.logout = function() {
+    Auth.logout();
+    $location.path('/');
+  };
+
+  $scope.isActive = function(route) {
+    return route === $location.path();
+  };
+  $scope.getUserImage = function() {
+    var guestImgPath = '/assets/images/guest.png';
+    return $scope.isLoggedIn() ? $scope.getCurrentUser().google.image.url : guestImgPath;
+  };
+
+  $scope.doPersonality = function(personalItem) {
+    if(personalItem === 'Log out') { $scope.logout(); }
+  };
+
+})
 .controller('SidenavCtrl', function ($scope, $mdSidenav, $log) {
   $scope.close = function () {
     $mdSidenav('left').close().then(function () {
