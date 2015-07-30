3183fe54527b3353ea861de9ebf034190dbc803a
jonghoon <hagfish1210@gmail.com>
2015-07-30 12:35:56 +0900

    더보기 기능 완성
    [Finishes|Delivers #99200640]

--- client/app/class/class.controller.js
+++ client/app/class/class.controller.js
@@ -2,8 +2,9 @@
 
 
 // 재생목록에 대한 정보 받아오기 (title, channelTitle, description)
-angular.module('learntubeApp')
+var app = angular.module('learntubeApp')
 .controller('ClassCtrl', function($scope, $http, $stateParams) {
+
   $http.get('https://www.googleapis.com/youtube/v3/playlists', {
     params: {
       key: 'AIzaSyBUuJS30-hhEY8f_kMF3K3rX4qe_bkY3V8',
@@ -13,36 +14,76 @@ angular.module('learntubeApp')
   }).success(function(response) {
     $scope.classe = response.items[0];
     console.log($scope.classe);
-  });
 
-// 재생목록 아이템에 대한 정보 받아오기 (position, title)
-$http.get('https://www.googleapis.com/youtube/v3/playlistItems', {
+    $scope.desc = $scope.classe.snippet.description;
+
+    // show function구현
+    //var change = true;    
+
+    $scope.content="view more";
+
+    $scope.change = function(){
+      //$scope.show = ($scope.show) ? false : true;
+      $scope.show = !$scope.show;
+
+        if($scope.show === false){
+          $scope.content="view more";
+        }else{
+          $scope.content="simple view";
+        }
+        return $scope.show; 
+
+        };
+
+    });
+
+  
+
+// 채널에 대한 정보 받아오기 (title, thumbnail, description)
+
+  $http.get('https://www.googleapis.com/youtube/v3/channels', {
     params: {
       key: 'AIzaSyBUuJS30-hhEY8f_kMF3K3rX4qe_bkY3V8',
       part: 'snippet',
-      playlistId: 'PL9B61DEF63FC19BD9',
-      maxResults: '10'
+      id: 'UCzw-C7fNfs018R1FzIKnlaA'
     }
   }).success(function(response) {
-    $scope.playlist_item = response.items;
-    console.log($scope.playlist_item);
-    
+    $scope.channel = response.items[0];
+    console.log($scope.channel);
+  });
+
+// 재생목록 아이템에 대한 정보 받아오기 (thumbnail, position, title)
+$http.get('https://www.googleapis.com/youtube/v3/playlistItems', {
+  params: {
+    key: 'AIzaSyBUuJS30-hhEY8f_kMF3K3rX4qe_bkY3V8',
+    part: 'snippet',
+    playlistId: 'PL9B61DEF63FC19BD9',
+    maxResults: '10'
+  }
+}).success(function(response) {
+  $scope.playlist_item = response.items;
+  console.log($scope.playlist_item);
+
+    // position == 0인 동영상의 ID가져오기
+    $scope.firstVideoId = $scope.playlist_item[0].snippet.resourceId.videoId;
+
+
     // Master GURU's beautiful code!
     var foo = $scope.playlist_item.map(function(el) {
-        return el.snippet.resourceId.videoId;
+      return el.snippet.resourceId.videoId;
     }).join(',');
     console.log(foo);
 
             // 비디오 아이템에 대한 정보 받아오기 (duration)
             $http.get('https://www.googleapis.com/youtube/v3/videos', {
-                params: {
-                  key: 'AIzaSyBUuJS30-hhEY8f_kMF3K3rX4qe_bkY3V8',
-                  part: 'contentDetails',
-                  id: foo
-                }
-              }).success(function(response) {
-                $scope.playlist_info = response.items;
-                console.log($scope.playlist_info);
+              params: {
+                key: 'AIzaSyBUuJS30-hhEY8f_kMF3K3rX4qe_bkY3V8',
+                part: 'contentDetails',
+                id: foo
+              }
+            }).success(function(response) {
+              $scope.playlist_info = response.items;
+              console.log($scope.playlist_info);
 
 
                 // 필요한 정보 (thumbnail, position, title, duration을 얻기 위한 객체 배열생성)
@@ -61,21 +102,16 @@ $http.get('https://www.googleapis.com/youtube/v3/playlistItems', {
                 }
                 console.log($scope.lecArr);
 
-
-
               });
 
-  
-
-
-  });
+          });
 
 // 새로운 배열을 만들어서, 그걸 repeat돌리자
 // 타이틀을 빼오고, 재생시간을 빼와서 object배열로. 
 
+                
+
+               
 
 });
-
-
-
 
