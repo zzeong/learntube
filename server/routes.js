bdbdbb72cd16125916a90740fd881ac750f57714
Jinyoung Kim <ligeek49@gmail.com>
2015-08-06 11:47:54 +0900

    remove legacy api from generator

--- server/routes.js
+++ server/routes.js
@@ -10,7 +10,6 @@ var path = require('path');
 module.exports = function(app) {
 
   // Insert routes below
-  app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
   app.use('/api/notes', require('./api/note'));
 
