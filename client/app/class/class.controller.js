da73635105c287a540954d7fa0f358fb300619b7
jonghoon <hagfish1210@gmail.com>
2015-08-07 16:48:02 +0900

    user can check total playtime(duration) in class summary page
    [Finishes|Delivers #99200674]

--- client/app/class/class.controller.js
+++ client/app/class/class.controller.js
@@ -1,5 +1,3 @@
-'use strict';
-
 
 angular.module('learntubeApp')
 .controller('ClassCtrl', function($scope, $http, $stateParams, $state) {
@@ -9,104 +7,127 @@ $http.get('https://www.googleapis.com/youtube/v3/playlists', {
   params: {
     key: 'AIzaSyBUuJS30-hhEY8f_kMF3K3rX4qe_bkY3V8',
     part: 'snippet',
-    id: 'PLgyxPQqhtaiu2smdVB7JO5zlh99yTmp35'
+    id: 'PLUBHG05jxN4XEfc9Oj8sNwsquFoyfNMaf'
   }
 }).success(function(response) {
   $scope.classe = response.items[0];
-  console.log($scope.classe);
-
   $scope.desc = $scope.classe.snippet.description;
 
-  
+  getLectureList();
 
-});
-
-
-
-// 채널에 대한 정보 받아오기 (title, thumbnail, description)
-
-$http.get('https://www.googleapis.com/youtube/v3/channels', {
-  params: {
-    key: 'AIzaSyBUuJS30-hhEY8f_kMF3K3rX4qe_bkY3V8',
-    part: 'snippet',
-    id: 'UCzw-C7fNfs018R1FzIKnlaA'
-  }
-}).success(function(response) {
-  $scope.channel = response.items[0];
-  console.log($scope.channel);
 });
 
 
 // 재생목록 아이템에 대한 정보 받아오기 (thumbnail, position, title)
+
+$scope.lecArr = [];        
+
+function getLectureList(pageToken){
 $http.get('https://www.googleapis.com/youtube/v3/playlistItems', {
   params: {
     key: 'AIzaSyBUuJS30-hhEY8f_kMF3K3rX4qe_bkY3V8',
     part: 'snippet',
-    playlistId: 'PLgyxPQqhtaiu2smdVB7JO5zlh99yTmp35',
-    maxResults: '30' 
+    playlistId: 'PLUBHG05jxN4XEfc9Oj8sNwsquFoyfNMaf',
+    maxResults: '10' ,
+    pageToken: pageToken
   }
-}).success(function(response) {
-  $scope.playlistItem = response.items;
-  console.log($scope.playlistItem);
-
-    // position == 0인 동영상의 ID가져오기
-    $scope.firstVideoId = $scope.playlistItem[0].snippet.resourceId.videoId;
-
+}).success(function(response1) {
+  $scope.playlistItem = response1.items;
+  console.log(response1);
 
     // Master GURU's beautiful code!
-    // (재생목록에 속한 모든 동영상의 아이디를 ,로 구분하여 foo에 저장)
+    // (재생목록에 속한 모든 동영상의 아이디를 ,로 구분하여 list에 저장)
     var list = $scope.playlistItem.map(function(el) {
       return el.snippet.resourceId.videoId;
     }).join(',');
     console.log(list);
+
+
 
             // 비디오 아이템에 대한 정보 받아오기 (duration)
             // (비공개 동영상도 여기까지는 전달된다)
             $http.get('https://www.googleapis.com/youtube/v3/videos', {
               params: {
                 key: 'AIzaSyBUuJS30-hhEY8f_kMF3K3rX4qe_bkY3V8',
-                part: 'contentDetails',
+                part: 'contentDetails, snippet',
                 id: list
               }
-            }).success(function(response) {
-              $scope.playlistInfo = response.items;
-              console.log($scope.playlistInfo);
-
+            }).success(function(response2) {
+              $scope.playlistInfo = response2.items;
+              
 
                 // 필요한 정보 (thumbnail, position, title, duration을 얻기 위한 객체 배열생성)
-                $scope.lecArr = [];
-
                 for(var idx=0; idx<$scope.playlistItem.length; idx++){
 
                   $scope.lecObj  = {};
 
                   if($scope.playlistItem[idx].snippet.description === 'This video is private.'){
                     $scope.lecObj.thumbnail = 'http://static-2.nexusmods.com/15/mods/130/images/thumbnails/59126-0-1433258627.png';
-                    $scope.lecObj.position =  $scope.playlistItem[idx].snippet.position;
-                    $scope.lecObj.lectureTitle = 'Private Video';
+                    $scope.lecObj.lectureTitle = '<< Private Video >>';
                     $scope.lecObj.duration = ''; 
-                    console.log('비디오 에러 발생! : ' + (idx+1) + '번째 동영상');
-                  }/*else if($scope.playlistInfo[idx].contentDetails.duration === ){
-                  
-                  }*/else{                  
+                    $scope.lecObj.index = '';
+                    $scope.lecObj.date = $scope.playlistInfo[idx].snippet.publishedAt;
+                    console.log('비공개 비디오 발생! : ' + (idx+1) + '번째 동영상');
+                  }else{                  
                     $scope.lecObj.thumbnail = $scope.playlistItem[idx].snippet.thumbnails.default.url;
-                    $scope.lecObj.position =  $scope.playlistItem[idx].snippet.position;
                     $scope.lecObj.lectureTitle = $scope.playlistItem[idx].snippet.title;
                     $scope.lecObj.duration = $scope.playlistInfo[idx].contentDetails.duration;
                     $scope.lecObj.videoId = $scope.playlistItem[idx].snippet.resourceId.videoId;
+                    $scope.lecObj.index = '';
+                    $scope.lecObj.date = $scope.playlistInfo[idx].snippet.publishedAt;
                   }
 
+                  //lecArr 구성
                   $scope.lecArr.push($scope.lecObj);
-
+                  //console.log($scope.lecArr.length);
                 }
-                console.log($scope.lecArr);
+
 
               });
 
+              // 전체 재생시간 변수생성
+              var totalDuration=0;
+
+             // 동영상 position 부여
+             for(var k=0; k<$scope.lecArrSorting.length; k++){
+              
+                      $scope.lecArrSorting[k].index = k+1;
+                    
+                      // 전체 재생시간 구하기
+                      // ※ 정규표현식에 대한 공부가 필요하다 ※
+                      var time = $scope.lecArrSorting[k].duration;
+                      time = /PT([0-9]?.)M([0-9]?.)S/.exec(time);
+                      //console.log(time);
+
+                      var min = time[1];
+                      var sec = time[2];
+
+
+                      totalDuration = totalDuration + parseInt(min)*60 + parseInt(sec);
+
+              }
+
+
+              $scope.totalDurationObj = {};
+              $scope.totalDurationObj.min = parseInt(totalDuration/60);
+              $scope.totalDurationObj.sec = (totalDuration%60);
+
+
+
+             
+            });
+
+
+      });
+
+
+}
+
+
+
+
 });
 
-// 새로운 배열을 만들어서, 그걸 repeat돌리자
-// 타이틀을 빼오고, 재생시간을 빼와서 object배열로. 
 
 
     // show function구현
@@ -129,7 +150,7 @@ $http.get('https://www.googleapis.com/youtube/v3/playlistItems', {
       if($scope[showProp] === false){
         $scope[contentProp]='view more';
       }else{
-        $scope[contentProp]='simple view';
+        $scope[contentProp]='view less';
       }
       return $scope[showProp]; 
 
