a4c37fcaf53a0ea6c04448d6c3481827ce709ad8
Jinyoung Kim <ligeek49@gmail.com>
2015-08-15 23:34:29 +0900

    style(test): clean up comments and rename variables

--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -2,16 +2,14 @@
 
 describe('Controller: MainCtrl', function () {
 
-  // load the controller's module
   beforeEach(module('learntubeApp'));
 
-  var MainCtrl, scope;
+  var MainCtrl, $scope;
 
-  // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
+    $scope = $rootScope.$new();
     MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+      $scope: $scope
     });
   }));
 
