5e0113b9f0947c28f09ab8625799ddffd1581e0d
Jinyoung Kim <ligeek49@gmail.com>
2015-08-21 16:01:25 +0900

    feat(page): implement adding class
    [Finishes|Delivers #99200602]

--- client/app/personal/uploaded/uploaded.controller.js
+++ client/app/personal/uploaded/uploaded.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('learntubeApp')
-.controller('UploadedContentsCtrl', function(GApi, GAuth, GoogleConst, $state, $scope) {
+.controller('UploadedContentsCtrl', function(GApi, GAuth, GoogleConst, $state, $scope, $mdDialog, $log) {
   $scope.go = $state.go;
 
   GAuth.login().then(function() {
@@ -15,4 +15,38 @@ angular.module('learntubeApp')
       });
     });
   });
+
+  $scope.showAdvanced = function(ev) {
+    $mdDialog.show({
+      controller: function($scope, $mdDialog) {
+        $scope.cancel = function() {
+          $mdDialog.cancel();
+        };
+        $scope.addClass = function(classe) {
+          GApi.executeAuth('youtube', 'playlists.insert', {
+            part: 'snippet,status',
+            resource: {
+              snippet: {
+                title: classe.title,
+                description: classe.desc,
+              },
+              status: {
+                privacyStatus: 'public',
+              },
+            },
+          }).then(function(res) {
+            $mdDialog.hide(res);
+          });
+        };
+      },
+      templateUrl: 'components/dialog/add-class.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose: true
+    })
+    .then(function(res) {
+      $scope.classes.unshift({ item: res });
+    });
+  };
+
 });
