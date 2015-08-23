5cb84e290917fbb62e956695f9dfa26a9feb75f9
Jinyoung Kim <ligeek49@gmail.com>
2015-08-23 19:50:40 +0900

    test(filter): add all tests about custom filter

---
+++ client/components/util/custom.filter.spec.js
@@ -0,0 +1,50 @@
+'use strict';
+
+describe('Filter: onlyPublic', function() {
+  beforeEach(module('learntubeApp'));
+  var filter;
+
+  beforeEach(inject(function($filter){
+    filter = $filter('onlyPublic');
+  }));
+
+  it('should return true when given undefined', function() {
+    expect(filter()).toEqual(true);
+  });
+
+  it('should return only public items when given items that have status.privacyStatus property', function() {
+    var items = [{
+      title: 'outgoing girl',
+      status: { privacyStatus: 'public', },
+    }, {
+      title: 'ashamed girl',
+      status: { privacyStatus: 'private', },
+    }];
+
+    expect(filter(items).length).toEqual(1);
+    expect(filter(items)[0].title).toEqual('outgoing girl');
+    expect(filter(items)[0].status.privacyStatus).toEqual('public');
+  });
+});
+
+describe('Filter: humanable', function() {
+  beforeEach(module('learntubeApp'));
+  var filter;
+
+  beforeEach(inject(function($filter){
+    filter = $filter('humanable');
+  }));
+
+  it('should return true when given undefined', function() {
+    expect(filter()).toEqual(true);
+  });
+
+  it('should return humanable format when given ISO-8601 duration', function() {
+    expect(filter('PT24M20S')).toEqual('24:20');
+    expect(filter('PT3H24M20S')).toEqual('03:24:20');
+  });
+
+  it('should return humanable format with minute unit when given ISO-8601 duration format which only has seconds', function() {
+    expect(filter('PT20S')).toEqual('00:20');
+  });
+});
