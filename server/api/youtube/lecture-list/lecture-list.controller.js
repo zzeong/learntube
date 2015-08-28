1b68409510828d235d44dcaf34b0dea66a8db8d0
Jinyoung Kim <ligeek49@gmail.com>
2015-08-28 17:14:49 +0900

    fix(api:youtube): chunk request to youtube
    The bug was occured by overflowed id capacity
    [#99200618]

--- server/api/youtube/lecture-list/lecture-list.controller.js
+++ server/api/youtube/lecture-list/lecture-list.controller.js
@@ -31,27 +31,48 @@ var getList = function(req) {
   });
 };
 
+var applyDuration = function(req, list) {
+  return new Promise(function(resolve, reject) {
+    var max = req.query.max || 50;
+    var chunkedList = _.chunk(list, max);
+
+    (function recurse(ci) {
+      var joinedId = chunkedList[ci].map(function(item) {
+        return item.snippet.resourceId.videoId;
+      }).join(',');
+
+      youtube.videos.list({
+        auth: config.google.serverKey,
+        part: 'contentDetails',
+        id: joinedId,
+        fields: 'items(contentDetails(duration))',
+      }, function(err, response) {
+        if(err) { return reject(err); }
+        
+        chunkedList[ci].forEach(function(item, i) {
+          _.assign(item, response.items[i]);
+        });
+
+        if(chunkedList.length === ci + 1) {
+          return resolve(list);
+        }
+
+        recurse(ci + 1);
+      }); 
+
+    })(0);
+  }); 
+};
+
 
 exports.index = function(req, res) {
-  getList(req).then(function(list) {
-    var joinedId = list.map(function(lecture) {
-      return lecture.snippet.resourceId.videoId;
-    }).join(',');
-
-    youtube.videos.list({
-      auth: config.google.serverKey,
-      part: 'contentDetails',
-      id: joinedId,
-      fields: 'items(contentDetails(duration))',
-    }, function(err, response) {
-      list.forEach(function(item, i) {
-        _.assign(item, response.items[i]);
-      });
-      return res.status(200).json(list);
-    }); 
-  }, function(err) {
-    return handleError(res, err);
-  });
+  getList(req)
+  .then(function(list) {
+    return applyDuration(req, list);
+  }, function(err) { return handleError(res, err); })
+  .then(function(list) {
+    return res.status(200).json(list); 
+  }, function(err) { return handleError(res, err); });
 };
 
 function handleError(res, err) {
