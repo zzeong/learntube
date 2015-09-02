6c5c79d09abd8695c97ed73e66d267cc772870b7
jonghoon <hagfish1210@gmail.com>
2015-09-02 14:30:54 +0900

    refactor(view): change css&html, add image&plugin

---
+++ client/components/util/readmore.directive.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('learntubeApp')
+.directive('hmRead', function () {
+  return {
+    restrict:'AE',
+    scope:{
+      hmtext : '@',
+      hmlimit : '@',
+      hmfulltext:'@',
+      hmMoreText:'@',
+      hmLessText:'@',
+      hmMoreClass:'@',
+      hmLessClass:'@'
+    },
+    templateUrl: 'components/util/readmore.tmpl.html',
+    transclude: true,
+    controller : function($scope){
+      $scope.toggleValue = function(){
+        $scope.hmfulltext = !$scope.hmfulltext;
+      };
+    }
+  };
+});
