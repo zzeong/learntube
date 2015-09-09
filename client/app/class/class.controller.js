b3cb1034dc8f69b5e87afb9552125a3d98642381
Jinyoung Kim <ligeek49@gmail.com>
2015-09-09 18:17:19 +0900

    refactor(controller): remove total duration of class
    Total duration will be provided by server API

--- client/app/class/class.controller.js
+++ client/app/class/class.controller.js
@@ -74,32 +74,6 @@ angular.module('learntubeApp')
       $scope.lectureList.forEach(function(item, i) {
         item.contentDetails = res.items[i].contentDetails; 
       }); 
-
-      $scope.totalPlaytime=0;
-
-      // 동영상 번호 부여 (오래된 동영상 -> 최근 동영상)
-      for(var i=0; i<$scope.lectureList.length; i++){
-        $scope.lectureList[i].index = i+1;
-        // 동영상 비교하기 위한 속성 부여
-        $scope.lectureList[i].highlight = false;
-
-        // duration에 humanable filter적용
-        $scope.lectureList[i].contentDetails.duration = $filter('humanable')($scope.lectureList[i].contentDetails.duration);
-
-        // duration을 split
-        var durationSplit = $scope.lectureList[i].contentDetails.duration.split(':');
-
-        // 분, 초를 모두 합해 totalPlaytime계산
-        $scope.totalPlaytime += parseInt(durationSplit[0])*60 + parseInt(durationSplit[1]);
-      }
-
-      // 실제 시, 분, 초 구하기
-      $scope.playtimeHour = parseInt($scope.totalPlaytime/3600);
-      $scope.playtimeMin = parseInt(($scope.totalPlaytime%3600)/60);
-      $scope.playtimeSec = parseInt(($scope.totalPlaytime%3600)%60);
-
     });
-
-
 
 });
