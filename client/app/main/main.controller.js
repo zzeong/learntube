e74ae150c8b6df766424f8e977f669d3bc96e3f9
Jinyoung Kim <ligeek49@gmail.com>
2015-09-20 23:13:57 +0900

    feat(page): show top rated classes
    [Finishes|Delivers #99200622]
    [Finishes|Delivers #99200624]

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,6 +1,23 @@
 'use strict';
 
 angular.module('learntubeApp')
-.controller('MainCtrl', function($scope, $state) {
+.controller('MainCtrl', function($scope, $state, GApi, GoogleConst, $http) {
   $scope.go = $state.go;
+
+  $http.get('/api/classes/get-tops', {
+    params: { num: 6 } 
+  })
+  .then(function(res) {
+    var params = {
+      key: GoogleConst.browserKey,
+      part: 'snippet',
+      id: res.data.map(function(d) { return d.playlistId; }).join(','),
+      maxResults: 20
+    };
+
+    return GApi.execute('youtube', 'playlists.list', params);
+  })
+  .then(function(res) {
+    $scope.classes = res.items; 
+  });
 });
