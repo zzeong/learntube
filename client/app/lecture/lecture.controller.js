27dc85741899edeef47dadd5b450ab9b34432512
Jinyoung Kim <ligeek49@gmail.com>
2015-09-22 11:32:07 +0900

    style(page): reindent

--- client/app/lecture/lecture.controller.js
+++ client/app/lecture/lecture.controller.js
@@ -23,144 +23,144 @@ angular.module('learntubeApp')
     $scope.publishedDate = ($scope.item.snippet.publishedAt).substring(0,10);
   });
 
-$scope.details = false;
+  $scope.details = false;
 
-$scope.showDetails = function(){
-  console.log('here is showDetails');
-  $scope.details = !$scope.details;
-};
+  $scope.showDetails = function(){
+    console.log('here is showDetails');
+    $scope.details = !$scope.details;
+  };
 
-var textToFile = function(text) {
-  return new Blob([text], {type: 'text/html'});
-};
+  var textToFile = function(text) {
+    return new Blob([text], {type: 'text/html'});
+  };
 
-var keepNoteSoundly = function(note, src) {
-  if(typeof src !== 'undefined') { _.assign(note, src); }
+  var keepNoteSoundly = function(note, src) {
+    if(typeof src !== 'undefined') { _.assign(note, src); }
 
-  if(!_.has(note, 'isEditing')) {
-    note.isEditing = false;
-  }
-  return note;
-};
-
-if($scope.isLoggedIn()) {
-  NoteAPI.query({ videoId: $scope.videoId }, function(notes) {
-    $scope.notes = notes.map(function(note) {
-      return keepNoteSoundly(note);
-    });
-  });
-}
-
-var initializeNotePanel = function(type) {
-  if(type === 'file') {
-    $scope.noteFile = undefined;
-    if($scope.isFileUploadOn) {
-      $scope.toggleFileUpload();
+    if(!_.has(note, 'isEditing')) {
+      note.isEditing = false;
     }
-  } else if(type === 'editor') {
-    $scope.noteContents = '';
-    if($scope.isEditorOn) {
-      $scope.toggleEditor();
-    }
-  } else {
-    return;
-  }
-};
+    return note;
+  };
 
-$http.get('/api/others-notes', {
-  params: { videoId: $scope.videoId }
-})
-.then(function(res) {
-  $scope.othersNotes = res.data;
-});
-
-$scope.completeLecture = function() {
-  $http.post('/api/users/' + $scope.getCurrentUser()._id + '/classes/', {
-    userId: $scope.getCurrentUser()._id
-  }).success(function(response) {
-    var classe = response;
-    $http.post('/api/users/' + $scope.getCurrentUser()._id + '/classes/' + classe._id + '/lectures/', {
-      videoId: $scope.videoId
-    }).success(function() {
-      $log.info('Saved Lecture');  
+  if($scope.isLoggedIn()) {
+    NoteAPI.query({ videoId: $scope.videoId }, function(notes) {
+      $scope.notes = notes.map(function(note) {
+        return keepNoteSoundly(note);
+      });
     });
-  });
-};
+  }
 
-$scope.toggleEditor = function() {
-  $scope.isEditorOn = !$scope.isEditorOn; 
-};
-$scope.toggleFileUpload = function() {
-  $scope.isFileUploadOn = !$scope.isFileUploadOn; 
-};
+  var initializeNotePanel = function(type) {
+    if(type === 'file') {
+      $scope.noteFile = undefined;
+      if($scope.isFileUploadOn) {
+        $scope.toggleFileUpload();
+      }
+    } else if(type === 'editor') {
+      $scope.noteContents = '';
+      if($scope.isEditorOn) {
+        $scope.toggleEditor();
+      }
+    } else {
+      return;
+    }
+  };
 
-$scope.editNote = function(note) {
-  NoteAPI.getContents({ nid: note._id }, function(res) {
-    note.contents = res.contents;
-    note.isEditing = true;
-  });
-};
-
-$scope.cancelEditing = function(note) {
-  note.isEditing = false; 
-};
-
-$scope.deleteNote = function(note) {
-  NoteAPI.remove({ nid: note._id }, function() {
-    _.remove($scope.notes, { _id: note._id });
-  });
-};
-
-$scope.updateNote = function(note) {
-  var file = textToFile(note.contents);
-
-  Upload.upload({
-    url: '/api/users/' + Auth.getCurrentUser()._id + '/notes/' + note._id,
-    method: 'PUT',
-    file: file
+  $http.get('/api/others-notes', {
+    params: { videoId: $scope.videoId }
   })
   .then(function(res) {
-    $scope.notes = $scope.notes.map(function(note) {
-      if(note._id === res.data._id) { return keepNoteSoundly(res.data); }
-      return note;
+    $scope.othersNotes = res.data;
+  });
+
+  $scope.completeLecture = function() {
+    $http.post('/api/users/' + $scope.getCurrentUser()._id + '/classes/', {
+      userId: $scope.getCurrentUser()._id
+    }).success(function(response) {
+      var classe = response;
+      $http.post('/api/users/' + $scope.getCurrentUser()._id + '/classes/' + classe._id + '/lectures/', {
+        videoId: $scope.videoId
+      }).success(function() {
+        $log.info('Saved Lecture');  
+      });
     });
-  });
-};
+  };
 
-$scope.doneNote = function(type, file) {
-  if(type === 'editor') {
-    file = textToFile(file);
-  }
+  $scope.toggleEditor = function() {
+    $scope.isEditorOn = !$scope.isEditorOn; 
+  };
+  $scope.toggleFileUpload = function() {
+    $scope.isFileUploadOn = !$scope.isFileUploadOn; 
+  };
 
-  Upload.upload({
-    url: '/api/users/' + Auth.getCurrentUser()._id + '/notes',
-    method:'POST',
-    fields: {
-      videoId: $scope.videoId,
-      playlistId: $scope.playlistId,
-      type: type
-    },
-    file: file
-  })
-  .then(function(res) {
-    var note = keepNoteSoundly(res.data);
-    $scope.notes.push(note);
+  $scope.editNote = function(note) {
+    NoteAPI.getContents({ nid: note._id }, function(res) {
+      note.contents = res.contents;
+      note.isEditing = true;
+    });
+  };
 
-    initializeNotePanel(type);
-  });
-};
+  $scope.cancelEditing = function(note) {
+    note.isEditing = false; 
+  };
 
-$scope.shouldBeEmbedded = function(note) {
-  if(note.resourceType.match(/^text\//)) {
-    return true;
-  }
-  return false;
-};
+  $scope.deleteNote = function(note) {
+    NoteAPI.remove({ nid: note._id }, function() {
+      _.remove($scope.notes, { _id: note._id });
+    });
+  };
+
+  $scope.updateNote = function(note) {
+    var file = textToFile(note.contents);
+
+    Upload.upload({
+      url: '/api/users/' + Auth.getCurrentUser()._id + '/notes/' + note._id,
+      method: 'PUT',
+      file: file
+    })
+    .then(function(res) {
+      $scope.notes = $scope.notes.map(function(note) {
+        if(note._id === res.data._id) { return keepNoteSoundly(res.data); }
+        return note;
+      });
+    });
+  };
+
+  $scope.doneNote = function(type, file) {
+    if(type === 'editor') {
+      file = textToFile(file);
+    }
+
+    Upload.upload({
+      url: '/api/users/' + Auth.getCurrentUser()._id + '/notes',
+      method:'POST',
+      fields: {
+        videoId: $scope.videoId,
+        playlistId: $scope.playlistId,
+        type: type
+      },
+      file: file
+    })
+    .then(function(res) {
+      var note = keepNoteSoundly(res.data);
+      $scope.notes.push(note);
+
+      initializeNotePanel(type);
+    });
+  };
+
+  $scope.shouldBeEmbedded = function(note) {
+    if(note.resourceType.match(/^text\//)) {
+      return true;
+    }
+    return false;
+  };
 
 
-$scope.getUserImgPath = function(user) {
-  var guestImgPath = '/assets/images/guest.png';
-  return _.has(user, 'google') ? user.google.image.url : guestImgPath;   
-};
+  $scope.getUserImgPath = function(user) {
+    var guestImgPath = '/assets/images/guest.png';
+    return _.has(user, 'google') ? user.google.image.url : guestImgPath;   
+  };
 });
 
