62d378c8c7bdb6892b9eb69ef8bf774578fd00f2
Jinyoung Kim <ligeek49@gmail.com>
2015-09-24 11:46:25 +0900

    style(page): reindent

--- client/app/personal/watched/lecture-list/lecture-list.js
+++ client/app/personal/watched/lecture-list/lecture-list.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('learntubeApp')
-  .config(function ($stateProvider) {
-    $stateProvider.state('WatchedLectureList', {
-        url: '/watched/lecture-list/:pid',
-        templateUrl: 'app/personal/watched/lecture-list/lecture-list.html',
-        controller: 'WatchedLectureListCtrl',
-      });
+.config(function ($stateProvider) {
+  $stateProvider.state('WatchedLectureList', {
+    url: '/watched/lecture-list/:pid',
+    templateUrl: 'app/personal/watched/lecture-list/lecture-list.html',
+    controller: 'WatchedLectureListCtrl',
   });
+});
