ba7b01f32fce959061d92b407ebed6fd2a1acb46
Jonghoon Kim <hagfish1210@gmail.com>
2015-09-25 16:39:04 +0900

    feat(page): visualize summary info about each class
    [Finishes|Delivers #99200632]

--- client/app/personal/watched/watched.controller.js
+++ client/app/personal/watched/watched.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('learntubeApp')
-.controller('WatchedContentsCtrl', function($scope, $http, ClassAPI, $state, GoogleConst, GApi) {
+.controller('WatchedContentsCtrl', function($scope, $http, ClassAPI, NoteAPI, $state, $log, GoogleConst, GApi) {
   $scope.go = $state.go;
 
   ClassAPI.query(function(response) {
@@ -13,21 +13,68 @@ angular.module('learntubeApp')
 
     GApi.execute('youtube', 'playlists.list', {
       key: GoogleConst.browserKey,
-      part: 'snippet',
+      part: 'snippet,contentDetails',
       id: playlistIds,
-      fields: 'items(snippet(title,thumbnails))',
+      fields: 'items(snippet(title,thumbnails),contentDetails)',
     })
     .then(function(responseFromYT) {
       $scope.classes = $scope.classes.map(function(classe, i) {
         classe.item = responseFromYT.items[i];
+        classe.graph = false;
+        classe.imgWidth = 0;
+        classe.imgHeight = 0;
+        classe.numberOfWatched = 0;
+        classe.numberOfNote = 0;
+        classe.percentage = 0;
+        console.log(classe);
         return classe;
       });
     });
   });
 
+
+
   $scope.deleteClass = function(classe) {
     ClassAPI.remove({ cid: classe._id }, function() {
-        _.remove($scope.classes, classe);
+      _.remove($scope.classes, classe);
     });
   };
+
+  $scope.showGraph = function(classe){
+    var img = document.getElementById('thumbnail');
+    classe.imgWidth = img.width;
+    classe.imgHeight = img.height;
+    //console.log(img.width);
+    //console.log(img.height);
+
+
+    // DB에서 시청한 동영상 목록 가져오기 (seenLectures)
+    ClassAPI.query({playlistId: classe.playlistId}, function(response){
+      classe.numberOfWatched = response[0].lectures.length;
+      classe.percentage = ((classe.numberOfWatched / classe.item.contentDetails.itemCount) * 100).toPrecision(3);
+      //console.log((classe.numberOfWatched / classe.item.contentDetails.itemCount).toPrecision(3));
+    }, function(err){
+      $log.error(err);
+    });
+
+
+    // DB에서 필기 목록 가져오기 (Note)
+    NoteAPI.query({playlistId: classe.playlistId}, function(response){
+      classe.numberOfNote = response.length;
+    }, function(err){
+      $log.error(err);
+    });
+
+
+    var graphPosition = document.getElementById('progressText');
+    graphPosition.style.top = '50px';
+
+    classe.graph = !classe.graph;
+
+
+  };
+
+
+
+
 });
