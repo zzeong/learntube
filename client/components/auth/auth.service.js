701aeaf2e1f07d018ad5e3d3d8b47c4b1f384729
Jinyoung Kim <ligeek49@gmail.com>
2015-09-29 21:35:16 +0900

    style(parameter): remove unused param

--- client/components/auth/auth.service.js
+++ client/components/auth/auth.service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('learntubeApp')
-  .factory('Auth', function Auth($location, $rootScope, $http, User, $cookieStore, $q) {
+  .factory('Auth', function Auth($location, $rootScope, $http, User, $cookieStore) {
     var currentUser = {};
     if ($cookieStore.get('token')) {
       currentUser = User.get();
