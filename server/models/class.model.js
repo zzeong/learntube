28f69370e57870048325320a07dd3f44c66e7240
Jinyoung Kim <jkiimm9@gmail.com>
2015-11-25 11:11:18 +0900

    feat(server:model): add new class model

---
+++ server/models/class.model.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ClassSchema = new Schema({
+  categorySlug: { type: String, required: true },
+  playlistId: { type: String, required: true },
+  channelId: { type: String, required: true },
+  rate: Number,
+  views: Number,
+  registeredAt: { type: Date, default: Date.now },
+});
+
+module.exprts = mongoose.model('Class', ClassSchema);
