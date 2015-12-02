8d0e590b36531ce8b1c35e575e4cf222c9aa03fc
Jonghoon Kim <hagfish1210@gmail.com>
2015-12-02 11:19:30 +0900

    feat(controller): bring classes of each category
    1. Awesome 'get-each-top' / 2. res.data를 5개씩 나누기 위해 chunk-inefficient사용

--- client/app/category/all/category-controller.js
+++ client/app/category/all/category-controller.js
@@ -3,10 +3,29 @@
 angular.module('learntubeApp')
 .controller('CategoryAllCtrl', function ($scope, $http, Category) {
 
-  console.log(Category);
-  $scope.categories = [];
-  for (var i in Category.name) {
-    $scope.categories.push(Category.name[i].orig);
-  }
+  Object.defineProperty(Array.prototype, 'chunk_inefficient', {
+    value: function (chunkSize) {
+      var array = this;
+      return [].concat.apply([],
+        array.map(function (elem, i) {
+          return i % chunkSize ? [] : [array.slice(i, i + chunkSize)];
+        })
+      );
+    }
+  });
+
+  $http.get('/api/categories/get-each-top')
+  .then(function (res) {
+    for (var i in res.data) {
+      for (var k in Category.name) {
+        if (res.data[i].categorySlug === Category.name[k].slug) {
+          res.data[i].categorySlug = Category.name[k].orig;
+        }
+      }
+    }
+
+    $scope.classArr = res.data.chunk_inefficient(5);
+  })
+  .catch(console.error);
 
 });
