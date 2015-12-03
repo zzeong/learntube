db6b71cdc68e70188d3653ab4872bcdf6cecfbfa
Jonghoon Kim <hagfish1210@gmail.com>
2015-12-03 17:40:59 +0900

    feat(filter): add 'thousand -> k'  filter

--- client/components/util/custom-filter.js
+++ client/components/util/custom-filter.js
@@ -39,4 +39,22 @@ angular.module('learntubeApp')
   return function (string) {
     return string.replace(/\ /g, '-').toLowerCase();
   };
-});
+})
+.filter('thousandSuffix', function () {
+    return function (input, decimals) {
+      var exp = ['k', 'M', 'G', 'T', 'P', 'E'];
+      var suffixes = ['k', 'M', 'G', 'T', 'P', 'E'];
+
+      if (window.isNaN(input)) {
+        return null;
+      }
+
+      if (input < 1000) {
+        return input;
+      }
+
+      exp = Math.floor(Math.log(input) / Math.log(1000));
+
+      return (input / Math.pow(1000, exp)).toFixed(decimals) + suffixes[exp - 1];
+    };
+  });
