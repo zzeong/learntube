4a41f836091ffe0ad991a65e3590ca5c55a7af73
Jinyoung Kim <jkiimm9@gmail.com>
2015-12-08 00:39:22 +0900

    refactor(directive): modularize star-rate

--- client/app/category/all/category-controller.js
+++ client/app/category/all/category-controller.js
@@ -14,31 +14,6 @@ angular.module('learntubeApp')
         }
 
       }
-
-      if (res.data[i].rate < 1.5) {
-        res.data[i].rate = 10;
-      } else if (1.5 <= res.data[i].rate && res.data[i].rate < 2.5) {
-        res.data[i].rate = 20;
-      } else if (2.5 <= res.data[i].rate && res.data[i].rate < 3.5) {
-        res.data[i].rate = 30;
-      } else if (3.5 <= res.data[i].rate && res.data[i].rate < 4.5) {
-        res.data[i].rate = 40;
-      } else if (4.5 <= res.data[i].rate && res.data[i].rate < 5.5) {
-        res.data[i].rate = 50;
-      } else if (4.5 <= res.data[i].rate && res.data[i].rate < 5.5) {
-        res.data[i].rate = 50;
-      } else if (5.5 <= res.data[i].rate && res.data[i].rate < 6.5) {
-        res.data[i].rate = 60;
-      } else if (6.5 <= res.data[i].rate && res.data[i].rate < 7.5) {
-        res.data[i].rate = 70;
-      } else if (7.5 <= res.data[i].rate && res.data[i].rate < 8.5) {
-        res.data[i].rate = 80;
-      } else if (8.5 <= res.data[i].rate && res.data[i].rate < 9.5) {
-        res.data[i].rate = 90;
-      } else {
-        res.data[i].rate = 100;
-      }
-
     }
 
     $scope.classArr = _.chunk(res.data, 5);
