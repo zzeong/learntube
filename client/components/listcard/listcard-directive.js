ef82a11ed69dc2b415e5250aca981cccd6f2d977
Jinyoung Kim <jkiimm9@gmail.com>
2015-12-10 23:01:04 +0900

    refactor(view): make listcard compatible widely

---
+++ client/components/listcard/listcard-directive.js
@@ -0,0 +1,58 @@
+(function (angular) {
+  'use strict';
+
+  angular.module('learntubeApp')
+  .directive('ltListcard', LtClassDirective);
+
+  function LtClassDirective() {
+    return {
+      restrict: 'E',
+      scope: {
+        responsiveMorph: '=ltResponsiveMorph',
+      },
+      link: postLink,
+    };
+
+    function postLink(scope, elem) {
+      if (scope.responsiveMorph) {
+        elem.addClass('lt-listcard--morph');
+      }
+    }
+  }
+
+
+  angular.module('learntubeApp')
+  .directive('ltListcardMedia', LtListcardMedia);
+
+  function LtListcardMedia() {
+    return {
+      restrict: 'E'
+    };
+  }
+
+
+  angular.module('learntubeApp')
+  .directive('ltListcardContent', LtListcardContent);
+
+  function LtListcardContent() {
+    return {
+      restrict: 'E',
+      transclude: true,
+      template: '<div class="lt-listcard__info-box" ng-transclude></div>',
+    };
+  }
+
+  angular.module('learntubeApp')
+  .directive('ltListcardActions', LtListcardActions);
+
+  function LtListcardActions() {
+    return {
+      restrict: 'E',
+      compile: compile,
+    };
+
+    function compile(elem) {
+      elem.addClass('lt-listcard-actions');
+    }
+  }
+})(angular);
