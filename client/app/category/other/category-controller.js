35e1bcc532ef8fc3c8098b268a6b40efe83f2c34
Jinyoung Kim <jkiimm9@gmail.com>
2015-12-21 17:50:33 +0900

    feat(view): support classes of general categories with sorting
    [Finishes #108731794]

--- client/app/category/other/category-controller.js
+++ client/app/category/other/category-controller.js
@@ -1,5 +1,18 @@
-'use strict';
+(function (angular) {
+  'use strict';
 
-angular.module('learntubeApp')
-.controller('CategoryOtherCtrl', function () {
-});
+  angular.module('learntubeApp')
+  .controller('CategoryOtherCtrl', CategoryOtherCtrl);
+
+  function CategoryOtherCtrl($scope, $state, Category, $filter, $http) {
+    $scope.href = $state.href.bind(null);
+    $scope.category = _.find(Category.name, (name) => {
+      return _.isEqual($filter('urlSafely')(name.orig), $state.params.ctname);
+    });
+
+    $http.get('/api/categories', { params: { slug: $scope.category.slug } })
+    .then((res) => { $scope.classes = res.data; });
+  }
+
+  CategoryOtherCtrl.$inject = ['$scope', '$state', 'Category', '$filter', '$http'];
+})(angular);
