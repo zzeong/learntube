84b5c7813051522922a506a9834869cb4ddc391a
Jinyoung Kim <jkiimm9@gmail.com>
2016-01-15 11:26:42 +0900

    fix(mongoose): numeric error at limit

--- server/api/class/class.controller.js
+++ server/api/class/class.controller.js
@@ -4,10 +4,11 @@ var Class = require('../../models/class.model');
 
 exports.getTops = (req, res, next) => {
   if (!req.query.num) { return res.status(500).send('No required params'); }
+  let num = +req.query.num;
 
   Class.find({})
   .sort({ views: 'desc' })
-  .limit(req.query.num)
+  .limit(num)
   .exec()
   .then((classes) => res.status(200).json(classes))
   .catch(next);
