47260706bb3db7eda7b1bd7860bb58b66069a225
Jinyoung Kim <jkiimm9@gmail.com>
2016-01-26 10:31:04 +0900

    refactor(api@s): change to node structure convention

--- server/api/user/note/note.controller.js
+++ server/api/user/note/note.controller.js
@@ -1,35 +1,32 @@
 'use strict';
 
-var _ = require('lodash');
-var url = require('url');
-var Note = require('../../../models/note.model');
-var s3 = require('../../../components/s3');
-var kutil = require('../../../components/util');
+const _ = require('lodash');
+const url = require('url');
+const Note = require('../../../models/note.model');
+const s3 = require('../../../components/s3');
+const kutil = require('../../../components/util');
 
-exports.index = function (req, res) {
-  var data = _.assign(req.query, { userId: req.params.id });
+function index(req, res, next) {
+  let data = _.assign(req.query, { userId: req.params.id });
 
-  Note.find(data, function (err, notes) {
-    if (err) { return res.status(500).send(err); }
-    if (!notes) { return res.status(404).send('Not Found'); }
+  Note.find(data).exec()
+  .then((notes) => res.status(200).json(notes))
+  .catch(next);
+}
 
-    return res.status(200).json(notes);
-  });
-};
-
-exports.create = (req, res, next) => {
-  var hash = kutil.getUniqueHash();
-  var uploadPath = `/${req.user.email}/notes/${hash}`;
+function create(req, res, next) {
+  let hash = kutil.getUniqueHash();
+  let uploadPath = `/${req.user.email}/notes/${hash}`;
 
   s3.putFile(req.files.file.path, uploadPath, {
     'Content-Type': req.files.file.type,
     'x-amz-acl': 'public-read'
   })
   .then(() => {
-    var uploadUrl = s3.client.url(uploadPath);
+    let uploadUrl = s3.client.url(uploadPath);
     console.log('[S3]:PUT saved to %s', uploadUrl);
 
-    var note = new Note({
+    let note = new Note({
       userId: req.params.id,
       videoId: req.body.videoId,
       playlistId: req.body.playlistId,
@@ -42,19 +39,59 @@ exports.create = (req, res, next) => {
   })
   .then((note) => res.status(201).json(_.omit(note.toObject(), 'userId')))
   .catch(next);
-};
+}
 
-
-exports.show = function (req, res) {
-  Note.findById(req.params.nid, function (err, note) {
-    if (err) { return res.status(500).send(err); }
-    if (!note) { return res.status(404).send('Not Found'); }
-
+function show(req, res, next) {
+  Note.findById(req.params.nid).exec()
+  .then((note) => {
+    if (_.isNull(note)) { return res.status(404).send('Not Found'); }
     return res.status(200).json(note);
-  });
-};
+  })
+  .catch(next);
+}
 
-exports.getContents = (req, res, next) => {
+function update(req, res, next) {
+  Note.findById(req.params.nid)
+  .then((note) => {
+    if (_.isNull(note)) { return res.status(404).send('Not Found'); }
+
+    let uploadPath = url.parse(note.url).pathname;
+
+    return s3.putFile(req.files.file.path, uploadPath, {
+      'Content-Type': req.files.file.type,
+      'x-amz-acl': 'public-read',
+    })
+    .then(() => {
+      let uploadUrl = s3.client.url(uploadPath);
+      console.log('[S3]:PUT saved to %s', uploadUrl);
+
+      note.videoId = req.query.videoId || note.videoId;
+      note.playlistId = req.query.playlistId || note.playlistId;
+      note.type = req.query.type || note.type;
+
+      return note.save();
+    });
+  })
+  .then((note) => res.status(201).json(note))
+  .catch(next);
+}
+
+function destroy(req, res, next) {
+  Note.findById(req.params.nid)
+  .then((note) => {
+    if (_.isNull(note)) { return res.status(404).send('Not Found'); }
+    let uploadPath = url.parse(note.url).pathname;
+    return s3.deleteFile(uploadPath)
+    .then((res) => {
+      console.log('[S3]:DELETE', res.statusCode, res.headers);
+      return note.remove();
+    });
+  })
+  .then(() => res.status(204).send())
+  .catch(next);
+}
+
+function getContents(req, res, next) {
   Note.findById(req.params.nid).exec()
   .then((note) => {
     return s3.getFile(url.parse(note.url).pathname)
@@ -74,46 +111,13 @@ exports.getContents = (req, res, next) => {
     });
   })
   .catch(next);
-};
+}
 
-exports.update = function (req, res, next) {
-  Note.findById(req.params.nid)
-  .then((note) => {
-    if (!note) { return res.status(404).send('Not Found'); }
+exports.index = index;
+exports.create = create;
+exports.show = show;
+exports.update = update;
+exports.destroy = destroy;
 
-    var uploadPath = url.parse(note.url).pathname;
-
-    return s3.putFile(req.files.file.path, uploadPath, {
-      'Content-Type': req.files.file.type,
-      'x-amz-acl': 'public-read',
-    })
-    .then(() => {
-      var uploadUrl = s3.client.url(uploadPath);
-      console.log('[S3]:PUT saved to %s', uploadUrl);
-
-      note.videoId = req.query.videoId || note.videoId;
-      note.playlistId = req.query.playlistId || note.playlistId;
-      note.type = req.query.type || note.type;
-
-      return note.save();
-    });
-  })
-  .then((note) => res.status(201).json(note))
-  .catch(next);
-};
-
-exports.destroy = (req, res, next) => {
-  Note.findById(req.params.nid)
-  .then((note) => {
-    if (!note) { return res.status(404).send('Not Found'); }
-    let uploadPath = url.parse(note.url).pathname;
-    return s3.deleteFile(uploadPath)
-    .then((res) => {
-      console.log('[S3]:DELETE', res.statusCode, res.headers);
-      return note.remove();
-    });
-  })
-  .then(() => res.status(204).send())
-  .catch(next);
-};
+exports.getContents = getContents;
 
